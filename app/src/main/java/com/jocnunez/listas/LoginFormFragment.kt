5903c46cb9f641fa0ddd865184435f341639d35e
jocnunez <jose.nunnez@gmail.com>
2022-06-29 10:12:47 +0200

    feat: add login form fragment

---
+++ app/src/main/java/com/jocnunez/listas/LoginFormFragment.kt
@@ -0,0 +1,18 @@
+package com.jocnunez.listas
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LoginFormFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login_form, container, false)
+    }
+}
